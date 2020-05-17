bfba76176cc5cbf3f7501b524eb717aabc217b22
Philippe Mailly <philippe.mailly@college-de-france.fr>
2020-05-17 14:28:45 +0200

    manage rois from Omero

--- src/main/java/Mito_Morph_Omero.java
+++ src/main/java/Mito_Morph_Omero.java
@@ -4,10 +4,8 @@
  * Author Philippe Mailly
  */
 
-import static Mito_Utils.Mito_Processing.analyzeSkeleton;
 import static Mito_Utils.Mito_Processing.computeMitoParameters;
 import static Mito_Utils.Mito_Processing.find_nucleus2;
-import static Mito_Utils.Mito_Processing.getPopFromImage;
 import ij.*;
 import ij.io.FileSaver;
 import ij.plugin.PlugIn;
@@ -17,15 +15,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import mcib3d.geom.Objects3DPopulation;
 import mcib3d.image3d.ImageHandler;
-import mcib3d.image3d.ImageInt;
 import static Mito_Utils.Mito_Processing.flush_close;
-import static Mito_Utils.Mito_Processing.median_filter;
-import static Mito_Utils.Mito_Processing.randomColorPop;
-import static Mito_Utils.Mito_Processing.threshold;
 import static Mito_Utils.JDialogOmeroConnect.imageData;
 import static Mito_Utils.JDialogOmeroConnect.selectedDataset;
 import static Mito_Utils.JDialogOmeroConnect.selectedProject;
+import static Mito_Utils.Mito_Processing.analyzeSkeleton;
 import static Mito_Utils.Mito_Processing.clearOutSide;
+import static Mito_Utils.Mito_Processing.getPopFromImage;
+import static Mito_Utils.Mito_Processing.median_filter;
+import static Mito_Utils.Mito_Processing.threshold;
 import Mito_Utils.OmeroConnect;
 import static Mito_Utils.OmeroConnect.addFileAnnotation;
 import static Mito_Utils.OmeroConnect.gateway;
@@ -90,84 +88,81 @@ public class Mito_Morph_Omero implements PlugIn {
                         // find if roi exist and clearoutside
 
                         List<Roi> rois = Mito_Utils.OmeroConnect.getImageRois(image);
-                        
-                        /*
-                         * Open DAPI channel ch1
-                         */
                         String seriesName = image.getName();
+                        /*
+                         * Open channels
+                         */
+                        
+                        
+                        // DAPI channel ch1
                         System.out.println("Opening Nucleus channel");
-                        ImagePlus imgNuc = getImageZ(image, 1, 1, 0, sizeZ).getImagePlus();
-                        
-                        Objects3DPopulation nucPop = new Objects3DPopulation();
-                        if (!rois.isEmpty())
-                            nucPop = find_nucleus2(imgNuc, rois.get(0));
-                        else
-                            nucPop = find_nucleus2(imgNuc, null);
-                        
-                        int totalNucPop = nucPop.getNbObjects();
-                        System.out.println("Detected nucleus = "+totalNucPop);
-                        
-                        // tags nucleus
-                        ImageHandler imhNuc = ImageInt.wrap(imgNuc).createSameDimensions();
-                        randomColorPop(nucPop, imhNuc, true);
-                        // save image for nucleus population
-                        imhNuc.getImagePlus().setCalibration(cal);
-                        
-                        FileSaver ImgNucFile = new FileSaver(imhNuc.getImagePlus());
-                        String imageFile = rootName + "_" + seriesName + "_Nucleus_Objects.tif";
-                        ImgNucFile.saveAsTiff(tempDir + imageFile);
-                        imhNuc.closeImagePlus();
-                        flush_close(imgNuc);
-                        // Add image to dataset
-                        OmeroConnect.addImageToDataset(selectedProject, selectedDataset, tempDir, imageFile);
+                        ImagePlus imgNucOrg = getImageZ(image, 1, 1, 0, sizeZ).getImagePlus();
                         
                         // Open mito channel ch0
+                        System.out.println("Opening Mito channel");
                         ImagePlus imgMitoOrg = getImageZ(image, 1, 0, 0, sizeZ).getImagePlus();
                         
-                        // Find mitos
+                        //If no rois found take the whole image
+                        if (rois.isEmpty()) {
+                            System.out.println("No roi found, taking the whole image");
+                            rois.add(0, new Roi(0, 0, imgNucOrg.getWidth(), imgNucOrg.getHeight()));
+                        }
                         
-                        median_filter(imgMitoOrg, 1.5);
-                        IJ.run(imgMitoOrg, "Laplacian of Gaussian", "sigma=4 scale_normalised negate stack");
-                        threshold(imgMitoOrg, AutoThresholder.Method.RenyiEntropy, false, false);
-                                                
-                        if (!rois.isEmpty())
-                            clearOutSide(imgMitoOrg, rois.get(0));
+                        // for all rois
+                        for( int r = 0; r < rois.size(); r++) {
+                            Roi roi = rois.get(r);
+                            Objects3DPopulation nucPop = new Objects3DPopulation();
+                            imgNucOrg.setRoi(roi);
+                            ImagePlus imgNuc = imgNucOrg.duplicate();
+                            nucPop = find_nucleus2(imgNuc, roi);
+                            int totalNucPop = nucPop.getNbObjects();
+                            System.out.println("Roi " + (r+1)+" Detected nucleus = "+totalNucPop);
                         
+                            // Find mitos
+                            imgMitoOrg.setRoi(roi);
+                            ImagePlus imgMito = imgMitoOrg.duplicate();
+                            median_filter(imgMito, 1.5);
+                            IJ.run(imgMito, "Laplacian of Gaussian", "sigma=4 scale_normalised negate stack");
+                            threshold(imgMito, AutoThresholder.Method.RenyiEntropy, false, false);
+                            clearOutSide(imgMito, roi);
+
+                            Objects3DPopulation mitoPop = getPopFromImage(imgMito, cal);
+                            //objectsSizeFilter(minMito, maxMito, mitoPop, imgMitoOrg, false); 
+                            System.out.println("Mito pop = "+ mitoPop.getNbObjects());
+
+                            // Find mito network morphology
+                            // Skeletonize
+                            double[] skeletonParams = analyzeSkeleton(imgMito, tempDir + rootName);
+                                                    
+                            // Compute global Mito parameters
+                            // nb of mito, mean mito volume, skeleton parameters
+                            IJ.showStatus("Writing parameters ...");
+                            computeMitoParameters(nucPop.getNbObjects(), mitoPop, skeletonParams, rootName+seriesName, results);
                         
-                        Objects3DPopulation mitoPop = getPopFromImage(imgMitoOrg, cal);
-                        //objectsSizeFilter(minMito, maxMito, mitoPop, imgMitoOrg, false);
-                        System.out.println("Mito pop = "+ mitoPop.getNbObjects());
+                            // Save objects image
+                            ImageHandler imhMitoObjects = ImageHandler.wrap(imgMito).createSameDimensions();
+                            ImageHandler imhNucObjects = imhMitoObjects.duplicate();
+                            mitoPop.draw(imhMitoObjects, 255);
+                            nucPop.draw(imhNucObjects, 255);
+                            ImagePlus[] imgColors = {imhMitoObjects.getImagePlus(), null, imhNucObjects.getImagePlus()};
+                            ImagePlus imgObjects = new RGBStackMerge().mergeHyperstacks(imgColors, false);
+                            imgObjects.setCalibration(cal);
+                            IJ.run(imgObjects, "Enhance Contrast", "saturated=0.35");
+                            FileSaver ImgObjectsFile = new FileSaver(imgObjects);
+                            String imageFile = rootName + "_" + seriesName + "_Objects.tif";
+                            ImgObjectsFile.saveAsTiff(tempDir + imageFile);
+                            // add image to dataset
+                            OmeroConnect.addImageToDataset(selectedProject, selectedDataset, tempDir, imageFile);
                         
-                        // Find mito network morphology
-                        // Skeletonize
-                        double[] skeletonParams = analyzeSkeleton(imgMitoOrg, tempDir + rootName);
-                        
-                        
-                        // Compute global Mito parameters
-                        // nb of mito, mean mito volume, skeleton parameters
-                        IJ.showStatus("Writing parameters ...");
-                        computeMitoParameters(nucPop.getNbObjects(), mitoPop, skeletonParams, rootName+seriesName, results);
-                        
-                        // Save objects image
-                        ImageHandler imhMitoObjects = ImageHandler.wrap(imgMitoOrg).createSameDimensions();
-                        ImageHandler imhNucObjects = imhMitoObjects.duplicate();
-                        mitoPop.draw(imhMitoObjects, 255);
-                        nucPop.draw(imhNucObjects, 255);
-                        ImagePlus[] imgColors = {imhMitoObjects.getImagePlus(), null, imhNucObjects.getImagePlus()};
-                        ImagePlus imgObjects = new RGBStackMerge().mergeHyperstacks(imgColors, false);
-                        imgObjects.setCalibration(cal);
-                        IJ.run(imgObjects, "Enhance Contrast", "saturated=0.35");
-                        FileSaver ImgObjectsFile = new FileSaver(imgObjects);
-                        imageFile = rootName + "_" + seriesName + "_Objects.tif";
-                        ImgObjectsFile.saveAsTiff(tempDir + imageFile);
-                        // add image to dataset
-                        OmeroConnect.addImageToDataset(selectedProject, selectedDataset, tempDir, imageFile);
-                        
-                        flush_close(imgObjects);
-                        flush_close(imhMitoObjects.getImagePlus());
-                        flush_close(imhNucObjects.getImagePlus());
-                        flush_close(imgNuc);
+                            flush_close(imgObjects);
+                            flush_close(imhMitoObjects.getImagePlus());
+                            flush_close(imhNucObjects.getImagePlus());
+                            flush_close(imgNuc);
+                            flush_close(imgMito);
+                        }
+                        flush_close(imgNucOrg);
                         flush_close(imgMitoOrg);
+                            
                     } catch (ExecutionException ex) {
                         Logger.getLogger(Mito_Morph_Omero.class.getName()).log(Level.SEVERE, null, ex);
                     } catch (DSOutOfServiceException | DSAccessException | BigResult ex) {
